b23b1608a7fe5633e93be09aea3df1551c7e658a
mohamedfazilk <mohamedfazil961@gmail.com>
2021-09-28 15:02:36 +0530

    template view added

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, HomePageView
 
 urlpatterns = [
-  path('', homePageView, name='home') ]
+  path('', homePageView, name='home'),
+  path('home/', HomePageView.as_view(), name='home'),
+]
--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
 def homePageView(request):
     return HttpResponse('Hello, World!')
+
+class HomePageView(TemplateView):
+ template_name = 'pages/home.html'
