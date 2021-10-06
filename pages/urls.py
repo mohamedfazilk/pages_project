ab3aa391b6ad71c6137e917e0a63a3a17fd79383
mohamedfazilk <mohamedfazil961@gmail.com>
2021-10-06 15:37:17 +0530

    base.html extend to home and about page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import homePageView, HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-  path('', homePageView, name='home'),
-  path('home/', HomePageView.as_view(), name='home'),
-  path('about/', AboutPageView.as_view(), name='about')
+
+  path('', HomePageView.as_view(), name='home'),
+  path('about/', AboutPageView.as_view(), name='about'),
 ]
